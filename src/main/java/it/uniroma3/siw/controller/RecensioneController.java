3aba7a3177d685eba704378fc7fa7468c3472535
RitaTi7 <rita.tiani14@gmail.com>
2025-07-21 00:05:39 +0200

    inizio RecensioneController
    funzione e html per visualizzare l'elenco di recensioni associate a un libro. Inizio metodi di creazione

--- src/main/java/it/uniroma3/siw/controller/RecensioneController.java
+++ src/main/java/it/uniroma3/siw/controller/RecensioneController.java
@@ -3,13 +3,17 @@ package it.uniroma3.siw.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import it.uniroma3.siw.model.Libro;
 import it.uniroma3.siw.model.Recensione;
 import it.uniroma3.siw.service.LibroService;
 import it.uniroma3.siw.service.RecensioneService;
+import jakarta.validation.Valid;
 
 @Controller
 public class RecensioneController {
@@ -20,19 +24,34 @@ public class RecensioneController {
 	/*mostra tutte le recensioni di un dato libro*/
 	@GetMapping("/recensione/{idLibro}")
 	public String mostraRecensioni(@PathVariable("idLibro") Long id, Model model) {
-		model.addAttribute("recensioni", this.libroService.getLibroById(id).getRecensioni());
+		Libro libro= this.libroService.getLibroById(id);
+		model.addAttribute("recensioni", libro.getRecensioni());
+		model.addAttribute("libro", libro);
 		return "recensioniDiLibro.html";
 	}
 	
 	@GetMapping("/formNuovaRecensione/{idLibro}")				//non so se è necessario l'id del libro
 	public String formNuovaRecensione(@PathVariable("idLibro") Long id, Model model) {
 		model.addAttribute("recensione", new Recensione());
+		model.addAttribute("libro", this.libroService.getLibroById(id));
 		return "formNuovaRecensione.html";
 	}
 	
-	@PostMapping("/nuovaRecensione/{idLibro}")
-	public String nuovaRecensione(@PathVariable("idLibro") Long id, Model model) {
-		return "";
+	@PostMapping("/recensione/{idLibro}")
+	public String nuovaRecensione(@Valid @ModelAttribute("recensione") Recensione recensione, @PathVariable("idLibro") Long id, BindingResult bindingResult, Model model) {
+		if(bindingResult.hasErrors())
+			return "formNuovaRecensione.html";
+		else {
+			this.recensioneService.save(recensione);
+			model.addAttribute("recensione", recensione);
+			return "redirect:/recensione/" + id;
+		}
 	}
+	
+	//l'ultima funzione non funziona
+	//TODO: valuta se creare un metodo per visualizzare la singola recensione dall'elenco delle recensioni di un libro (magari cliccando sul titolo)
+	//cerca di capire dove devono essere create le recensioni
+	//occhio al fatto che quando crei una nuova recensione di un libro essa deve essere inserita nella lista del libro!!!!
+	//non sono elementi "indipendenti" come Libro e Autore che vengono soltanto inseriti nel db!!! (la Recensione ha anche un'associazione fissa!)
 
 }
--- src/main/java/it/uniroma3/siw/service/RecensioneService.java
+++ src/main/java/it/uniroma3/siw/service/RecensioneService.java
@@ -3,11 +3,16 @@ package it.uniroma3.siw.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import it.uniroma3.siw.model.Recensione;
 import it.uniroma3.siw.repository.RecensioneRepository;
 
 @Service
 public class RecensioneService {
 	
 	@Autowired RecensioneRepository recensioneRepository;
+	
+	public void save(Recensione recensione) {
+		this.recensioneRepository.save(recensione);
+	}
 
 }
